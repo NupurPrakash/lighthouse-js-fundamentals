3dc5a9d6f76b319c52f715c1497bb7f998cef44b
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-01 06:37:28 +0000

    Loopy light house

---
+++ loopy.js
@@ -0,0 +1,19 @@
+for(let i = 100 ; i <=200 ; i++)
+{
+  if(( i % 3 === 0 ) && (i % 4 === 0 ))
+  {
+    console.log('LoopyLightHouse');
+  }
+  else if(i % 3 === 0)
+  {
+    console.log("Loopy");
+  }
+  else if(i % 4 === 0)
+  {
+    console.log('Lighthouse');
+  }
+  else
+  {
+    console.log(i);
+  }
+}
